6d962b8ef80aa0bc5f297ec5c158d3f6e3ec37c5
Marcus Ottosson <konstruktion@gmail.com>
2017-06-28 09:06:48 +0100

    Improve update.bat

--- update.bat
+++ update.bat
@@ -2,9 +2,13 @@
 echo Updating Avalon Setup..
 echo+
 
+:: Discard any rouge changes to the codebase
 git submodule foreach git reset --hard
 git reset --hard
+
+:: Update, include any added submodules
 git pull origin master
+git submodule init
 git submodule update --recursive
 
 echo+
