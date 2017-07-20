e6d93c415970dc08193a3987416c75abf2c203e0
Marcus Ottosson <konstruktion@gmail.com>
2017-07-20 14:52:43 +0100

    Add backwards compatibility for fellow freelancers

--- mb-gui.bat
+++ mb-gui.bat
@@ -1,5 +1,10 @@
 @echo off
 
+:: Backwards compatibility
+if not defined AVALON_PROJECTS set AVALON_PROJECTS=%MINDBENDER_PROJECTS%
+if not defined AVALON_MONGO set AVALON_MONGO=%MINDBENDER_MONGO%
+if not defined AVALON_LOCATION set AVALON_LOCATION=%MINDBENDER_LOCATION%
+
 set AVALON_LABEL=Mindbender
 
 %~dp0bin\windows\python36\python.exe -u %~dp0avalon.py
